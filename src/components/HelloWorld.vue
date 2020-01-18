e2989505edbaf3699aaeb800eda2cc9718e16c59
howie <e0m1b@ugrad.cs.ubc.ca>
2020-01-17 16:53:17 -0800

    basic layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,75 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <button @click="trymethod" v-bind="btnText">{{btnText}}</button> 
+    <div v-if="refreshed" v-bind="refreshTime">Last time refreshed at: {{refreshTime}}</div>
+    <div id = "headlines" v-for = 'news in headlines' v-bind:key="news"> <!-- v-bind is binding an attribute to the specific tag-->
+      <news v-bind:msg="news"></news>
+    </div>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import news from "./news.vue";
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    abc: String,
+  },
+  data() {
+    return {
+      headlines: [],
+      received: [],
+      btnText: 'see what is new',
+      refreshTime: '',
+      refreshed: false,
+      date: new Date() 
+    }
+  },
+  methods: {
+    trymethod () {
+      this.refreshed = true;
+      this.date = new Date();
+      this.refreshTime = `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds()}`;
+      this.btnText = 'refresh the content';
+      axios.get('https://www.reddit.com/.json').then(res => {
+        // eslint-disable-next-line no-console
+        this.getPosts(res);
+      })
+    },
+
+    getPosts(res) {
+      res.data.data.children.forEach(post => {
+        // eslint-disable-next-line no-console
+        console.log(post);
+        this.headlines.push(post.data.title);
+
+        
+      })
+    }
+  },
+  components: {
+    news
   }
+
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
---
+++ src/components/news.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="newsTag">
+    <div id="title"> {{msg}}</div>
+    <div id="sentiment">1 {{sentiment}} </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'news',
+   props: {
+       msg: String
+   }
+}
+</script>
+
+<style scoped>
+  #title {
+    text-overflow: ellipsis;
+    width: 300px;
+    background-color: lightgoldenrodyellow;
+    font-size: 15px;
+    height: 100px;
+  }
+
+  #sentiment {
+    background-color: aqua;
+    width: 100px;
+    border-left : 5px solid grey;
+    padding: 20px;
+    font-size: 40px;
+    border-radius: 5px;
+  }
+  #newsTag {
+    margin: 20px auto;
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+  }
+</style>
